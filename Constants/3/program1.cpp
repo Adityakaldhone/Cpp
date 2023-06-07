80dd0144fcacf6bbc928a369adf73c1176e112fc
Adityakaldhone <adityakaldhone1357@gmail.com>
2023-06-07 08:19:16 +0530

    Constants

---
+++ Constants/3/program1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+int main() {
+
+	int y;
+
+	std::cout<<"Enter value"<<std::endl;
+
+	std::cin>>y;
+
+	const int x = y;
+	
+	std::cout<< x <<std::endl;
+
+}
+
+/*
+output:
+Enter value
+50
+50
+*/ 
